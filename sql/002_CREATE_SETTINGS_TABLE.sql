f90387669683f7789eebea22b58828bb81b54ec5
MaciejPadula <maciej.padula@wp.pl>
2024-03-30 20:40:22 +0100

    #18 change

--- sql/002_CREATE_SETTINGS_TABLE.sql
+++ sql/002_CREATE_SETTINGS_TABLE.sql
@@ -1,8 +1,9 @@
 IF OBJECT_ID(N'ScrumMaster.UserSettings', N'U') IS NULL
 BEGIN
     CREATE TABLE [ScrumMaster].[UserSettings] (
-        [UserId] varchar(255),
-        [TaigaAccess] nvarchar(max),
+        [UserId] nvarchar(255),
+        [TaigaAccessToken] nvarchar(1000),
+        [TaigaRefreshToken] nvarchar(1000),
         PRIMARY KEY ([UserId])
     )
 END

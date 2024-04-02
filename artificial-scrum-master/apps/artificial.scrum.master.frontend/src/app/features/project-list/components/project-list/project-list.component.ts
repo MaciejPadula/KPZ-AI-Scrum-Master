6e710f463a4a315f19a88b3fce131aaad10bd794
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-04-03 00:00:05 +0200

    init

---
+++ artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/features/project-list/components/project-list/project-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project-list.component.html',
+  styleUrl: './project-list.component.css',
+})
+export class ProjectListComponent {}

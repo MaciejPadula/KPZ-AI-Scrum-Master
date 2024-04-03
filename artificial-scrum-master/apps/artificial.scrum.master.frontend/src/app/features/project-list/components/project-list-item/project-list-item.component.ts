f2c624690bcb143aa68f23dfcc08d17dfd203490
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-04-03 17:09:41 +0200

    project list item onclick navigate

--- artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/features/project-list/components/project-list-item/project-list-item.component.ts
+++ artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/features/project-list/components/project-list-item/project-list-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { GetUserProjectsResponseElement } from '../../models/user-project';
 import { LinkComponent } from '../../../../shared/components/link/link.component';
 import { MaterialModule } from '../../../../shared/material.module';
@@ -14,7 +15,16 @@ export class ProjectListItemComponent {
   @Input()
   public userProject: GetUserProjectsResponseElement;
 
-  public getObjectUrl(id: number) {
-    return `/test/${id}`;
+  private readonly router = inject(Router);
+
+  public formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+
+  onComponentClick(id: number) {
+    this.router.navigate([`/projects/${id}`]);
   }
 }

660aaa86816011d778a80bc3fa29872511231dd0
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-04-18 13:02:22 +0200

    task component added tags

--- artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/shared/components/task-row/task-row.component.ts
+++ artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/shared/components/task-row/task-row.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { TaskRow } from './task-row';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-task-row',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule, AvatarComponent],
   templateUrl: './task-row.component.html',
 })
 export class TaskRowComponent {

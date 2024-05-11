11a2359f7b59bdc6152b4f3c52ec844d200dc4b2
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2024-05-11 23:07:42 +0200

    description editor draft

--- artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/features/tasks/components/task-details/task-details.component.ts
+++ artificial-scrum-master/apps/artificial.scrum.master.frontend/src/app/features/tasks/components/task-details/task-details.component.ts
@@ -1,10 +1,12 @@
 import {
   Component,
   computed,
+  ElementRef,
   inject,
   Inject,
   OnInit,
   signal,
+  ViewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -33,14 +35,28 @@ import { MarkdownEditorComponent } from '../../../../shared/components/markdown-
   ],
 })
 export class TaskDetailsComponent implements OnInit {
+  @ViewChild('taskDescription', { read: ElementRef })
+  taskDescription: ElementRef;
+  @ViewChild('descriptionDiff', { read: ElementRef })
+  descriptionDiff: ElementRef;
+  @ViewChild('editor', { read: ElementRef }) editor: ElementRef;
+
   private readonly formatDateService = inject(FormatDateService);
+  private readonly storyTaskSuggestionService = inject(
+    StoryTaskSuggestionService
+  );
 
   details = signal<TaskDetails | null>(null);
   error = signal<boolean>(false);
 
   isSuggestionsVisible = signal(false);
   suggestion = signal<GetStoryTaskSuggestion | null>(null);
-  storyTaskSuggestionService = inject(StoryTaskSuggestionService);
+
+  isEditorVisible = signal(false);
+  descriptionEditorValue = signal<string>('');
+
+  #isLoading = signal<boolean>(false);
+  public isLoading = this.#isLoading.asReadonly();
 
   formattedDate = computed(() =>
     this.formatDateService.formatDate(this.details()?.createdDate)
@@ -55,7 +71,10 @@ export class TaskDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.#httpClient.get<TaskDetails>(`/api/task/${this.#taskId}`).subscribe({
-      next: (response) => this.details.set(response),
+      next: (response) => {
+        this.details.set(response);
+        this.descriptionEditorValue.set(response.description ?? '');
+      },
       error: () => this.error.set(true),
     });
   }
@@ -64,13 +83,21 @@ export class TaskDetailsComponent implements OnInit {
     if (this.details() == null || !this.details()?.subject) {
       return;
     }
+    this.#isLoading.set(true);
+
     this.storyTaskSuggestionService
       .getTaskDescriptionSuggestion(
         this.details()?.subject ?? '',
         this.details()?.userStorySubject ?? null,
         this.details()?.description ?? null
       )
-      .pipe(finalize(() => this.isSuggestionsVisible.set(true)))
+      .pipe(
+        finalize(() => {
+          this.#isLoading.set(false);
+          this.isSuggestionsVisible.set(true);
+          setTimeout(() => this.scrollToElement(this.descriptionDiff), 50);
+        })
+      )
       .subscribe({
         next: (response) => this.suggestion.set(response),
         error: () => this.error.set(true),
@@ -79,5 +106,82 @@ export class TaskDetailsComponent implements OnInit {
 
   updateDescription(newValue: string) {
     console.log(newValue);
+  }
+
+  openDescriptionEditor() {
+    if (this.isEditorVisible()) {
+      setTimeout(() => this.isEditorVisible.set(false), 100);
+      setTimeout(() => this.scrollToElement(this.taskDescription), 50);
+      return;
+    }
+    setTimeout(() => this.isEditorVisible.set(true), 50);
+    setTimeout(() => this.scrollToElement(this.editor), 100);
+  }
+
+  rejectSuggestion() {
+    setTimeout(() => this.scrollToElement(this.taskDescription), 50);
+    this.isSuggestionsVisible.set(false);
+  }
+
+  replaceWithSuggestion() {
+    const details = this.details();
+    if (details == null) {
+      return;
+    }
+    details.description = this.suggestion()?.descriptionEditSuggestion ?? '';
+
+    this.isSuggestionsVisible.set(false);
+    if (!this.isEditorVisible()) {
+      this.isEditorVisible.set(true);
+    }
+  }
+
+  appendSuggestionToBack() {
+    const details = this.details();
+    if (details == null) {
+      return;
+    }
+    details.description = (details.description ?? '').concat(
+      '\n',
+      this.suggestion()?.descriptionEditSuggestion ?? ''
+    );
+
+    this.isSuggestionsVisible.set(false);
+    if (!this.isEditorVisible()) {
+      this.isEditorVisible.set(true);
+    }
+  }
+
+  appendSuggestionToFront() {
+    const details = this.details();
+    if (details == null) {
+      return;
+    }
+    details.description = (
+      this.suggestion()?.descriptionEditSuggestion ?? ''
+    ).concat('\n', details.description ?? '');
+
+    this.isSuggestionsVisible.set(false);
+    if (!this.isEditorVisible()) {
+      this.isEditorVisible.set(true);
+    }
+  }
+
+  resetDescription() {
+    throw new Error('Method not implemented.');
+  }
+  saveDescriptionChanges() {
+    throw new Error('Method not implemented.');
+  }
+
+  private scrollToElement(element: ElementRef) {
+    if (element && element.nativeElement) {
+      element.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      // nie działa
+      element.nativeElement.focus();
+    }
   }
 }
